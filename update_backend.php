ba75a75aa4be2e87e0ab3f88558a7d348eba2717
Sagar Dolui <sagar03d@gmail.com>
2017-09-10 21:24:54 +0530

    Fixed the image update issue

--- update_backend.php
+++ update_backend.php
@@ -2,6 +2,8 @@
 include('connection.php');
 if(isset($_REQUEST['submit'])){
 $id = $_SESSION['id'];
+$query3 = mysqli_query($connect,"SELECT image from user WHERE id='$id'");
+$row = mysqli_fetch_array($query3);
 	//get values given by user
 	$name = $_REQUEST['name2'];
 	$email = $_REQUEST['email'];
@@ -10,8 +12,8 @@ $id = $_SESSION['id'];
 	$skill = implode(",",$_REQUEST['sk']);
 	$gender = implode($_REQUEST['gender']);
 	$image = $_FILES['image']['name'];
-	$new = time()."_".rand(0,99)."_".$image;
-	$destination = "upload/".$new;
+	$new = $row['image'].$image;
+	$destination = $new;
 	$source = $_FILES['image']['tmp_name'];
 	move_uploaded_file($source,$destination);
 	$query2 = mysqli_query($connect,"UPDATE user SET name='$name', email='$email',birthday='$birthday',city='$city',skill='$skill',gender='$gender',image='$destination' WHERE id='$id'");
